5fe257a58a5675f9ae67d6784243e5f36d3551c4
Abraham Arce <xe1gy@gmail.com>
2016-05-14 17:05:20 +0000

    Flisol 2016 Part IV
    Signed-off-by: Abraham Arce <xe1gy@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import time
 
 import pyupm_grove as grove
-import pyupm_i2clcd as lcd
+#import pyupm_i2clcd as lcd
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 temperatura = grove.GroveTemp(0)
 luz = grove.GroveLight(1)
-display = lcd.Jhd1313m1(0, 0x3E, 0x62)
+#display = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
+def funcionTemperatura(bot, update):
+    grados = temperatura.value()
+    bot.sendMessage(update.message.chat_id, text='Temperatura ' + str(grados))
 
+def funcionEcho(bot, update):
+    bot.sendMessage(update.message.chat_id, text=update.message.text)
+
+if __name__ == '__main__':
+
+    updater = Updater("209701132:AAEBn3_8ZBN-Lk8l8kRnkLKegmjA-S5iPeQ")
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("temperatura", funcionTemperatura))
+    dp.add_handler(MessageHandler([Filters.text], funcionEcho))
+
+    updater.start_polling()
+    updater.idle()
+
+'''
 while True:
 
     grados = temperatura.value()
@@ -22,5 +42,5 @@ while True:
     display.write('Luz ' + str(luxes))
 
     time.sleep(5)
-
+'''
 del temperatura
