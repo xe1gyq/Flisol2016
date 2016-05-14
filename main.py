5c7dede4f652362cf65300876bc814f5d80ec697
Abraham Arce <xe1gy@gmail.com>
2016-05-14 16:04:57 +0000

    Flisol 2016 Part II
    Signed-off-by: Abraham Arce <xe1gy@gmail.com>

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 import pyupm_grove as grove
 import pyupm_i2clcd as lcd
 
-
 temperatura = grove.GroveTemp(0)
 display = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
 celsius = temperatura.value()
 
-display.setCursor(0,0)
-display.setColor(255, 0, 0)
+while True:
+    display.setCursor(0,0)
+    display.setColor(255, 0, 0)
+    display.write('Temperatura ' + str(celsius))
 
 del temperatura
